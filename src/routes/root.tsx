ad1fa4f079e53b2d53447a53a17a56568d675d32
Mark Carson <markcarson121@gmail.com>
2024-08-05 15:52:05 -0300

    implemented Formik on sign up form

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,44 +1,40 @@
-import { useState } from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
 
 import { firebaseSignUp } from "../firebase/AuthService";
 
 export function Root() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
-  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { type, value } = e.target;
-    if (type === "email") setEmail(value);
-    if (type === "password") setPassword(value);
-  };
-
   return (
     <>
       <h1>Fitness Tracker</h1>
-      <form className="flex flex-col">
-        <input
-          type="email"
-          placeholder="Email..."
-          value={email}
-          onChange={(v) => onChange(v)}
-        />
-        <input
-          type="password"
-          placeholder="Password..."
-          value={password}
-          onChange={(v) => onChange(v)}
-        />
-        <button
-          type="submit"
-          className="w-fit"
-          onClick={(e) => {
-            e.preventDefault();
-            firebaseSignUp({ email, password });
-          }}
-        >
-          Sign Up
-        </button>
-      </form>
+      <Formik
+        initialValues={{ email: "", password: "" }}
+        validate={(values) => {
+          const errors = {};
+          if (!values.email) {
+            errors.email = "Required";
+          } else if (
+            !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+          ) {
+            errors.email = "Invalid email address";
+          }
+          return errors;
+        }}
+        onSubmit={() => {
+          firebaseSignUp({ email, password });
+        }}
+      >
+        {({ isSubmitting }) => (
+          <Form className="flex flex-col border-2 w-fit">
+            <Field type="email" name="email" />
+            <ErrorMessage name="email" component="div" />
+            <Field type="password" name="password" />
+            <ErrorMessage name="password" component="div" />
+            <button className="w-fit" type="submit" disabled={isSubmitting}>
+              Submit
+            </button>
+          </Form>
+        )}
+      </Formik>
     </>
   );
 }
