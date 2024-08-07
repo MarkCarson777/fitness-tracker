1c36514e92ae24eabdc2d137f8b222071c2e2a19
Mark Carson <markcarson121@gmail.com>
2024-08-07 16:55:08 -0300

    added aria labels

--- src/views/LogIn/index.tsx
+++ src/views/LogIn/index.tsx
@@ -30,22 +30,26 @@ export function Login() {
           <>
             <Form className="flex flex-col w-fit gap-3">
               <div className="flex flex-col gap-1">
+                <label htmlFor="email">Email</label>
                 <FormInput
                   type="email"
                   name="email"
                   autoComplete="email"
                   placeholder="Email"
                   className="min-w-[320px] rounded-sm"
+                  aria-labelledby="email-label"
                 />
                 <FormError name="email" component="div" />
               </div>
               <div className="flex flex-col gap-1">
+                <label htmlFor="password">Password</label>
                 <FormInput
                   type="password"
                   name="password"
                   autoComplete="current-password"
                   placeholder="Password"
                   className="min-w-[320px] rounded-sm"
+                  aria-labelledby="password-label"
                 />
                 <FormError name="password" component="div" />
               </div>
@@ -53,20 +57,28 @@ export function Login() {
                 className="h-[48px] bg-[#3163d9]"
                 type="submit"
                 disabled={isSubmitting}
+                aria-live="polite"
               >
                 <span>Log In</span>
               </Button>
             </Form>
             <div className="flex gap-1 text-xs">
               <span>Don't have an account?</span>
-              <Link className="underline" to="/signup">
+              <Link
+                className="underline"
+                to="/signup"
+                aria-label="Sign up for an account"
+              >
                 Sign Up
               </Link>
             </div>
           </>
         )}
       </Formik>
-      <div className="absolute left-8 bottom-8 flex flex-col text-9xl">
+      <div
+        className="absolute left-8 bottom-8 flex flex-col text-9xl"
+        aria-label="Fit Tracker"
+      >
         <span>Fit</span>
         <span>Tracker.</span>
       </div>
